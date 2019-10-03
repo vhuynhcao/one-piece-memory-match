739c8bc3a2dee9c74ffbe692b2669240ec461734
Vicky Huynhcao <vicky89@gmail.com>
2019-10-02 21:47:11 -0700

    working modal minus close btn no bug

--- main.js
+++ main.js
@@ -3,34 +3,45 @@ $(document).ready(initializeApp);
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
+var max_matches = 2;
 
 function initializeApp(){
   $('.card').click(handleCardClick);
 }
 
 function hideCard() {
-  firstCardClicked.find('.front').removeClass('hidden');
-  secondCardClicked.find('.front').removeClass('hidden');
+  firstCardClicked.find('.back').removeClass('hidden');
+  secondCardClicked.find('.back').removeClass('hidden');
   firstCardClicked = null;
   secondCardClicked = null;
 }
 
+function allCardsMatched(){
+  if(matches === max_matches){
+    toggleModal();
+    $('main, .header').css('opacity', '.4');
+  }
+}
+
 function handleCardClick(event){
-  $(event.currentTarget).find('.front').addClass('hidden');
+  $(event.currentTarget).find('.back').addClass('hidden');
 
   if(firstCardClicked === null){
     firstCardClicked = $(event.currentTarget);
   } else {
     secondCardClicked = $(event.currentTarget);
-    if (firstCardClicked.find('.back').css('background-image') === secondCardClicked.find('.back').css('background-image')) {
+    if (firstCardClicked.find('.front').css('background-image') === secondCardClicked.find('.front').css('background-image')) {
       matches++;
+      allCardsMatched();
       firstCardClicked = null;
       secondCardClicked = null;
       console.log('card matches');
     } else {
         setTimeout(hideCard, 1500);
-
     }
   }
+}
 
+function toggleModal(){
+  $('.endModal').removeClass('hidden');
 }
